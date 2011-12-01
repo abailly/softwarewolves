55394ab34d72764e6790e34c8cc042ad6c2cea20
Arnaud Bailly <Arnaud.Bailly@polyspot.com>
2011-12-01 23:03:16 +0100

    story 2 seems to be working

--- Core.hs
+++ Core.hs
@@ -17,18 +17,20 @@ playWerewolf rand out v (E name f) = do
   input <- getLine
   let (cont, (o, v'))    = f v [input]
   input <- getLine
-  let (cont', (o', v'')) = run cont v' [input]
+  let (rand', killed) = villagersVote (1,head input) v' rand
+  let (cont', (o', v'')) = run cont v' [[killed]]
   case endGame v' of
     None       -> hPutStrLn out (concat $ intersperse "\n" (o ++ o')) >> 
-                  playWerewolf rand out v'' cont'
+                  playWerewolf rand' out v'' cont'
     Werewolves -> hPutStrLn out $ "the werewolves win"
     Villagers  -> hPutStrLn out $ "the villagers win"
 
 playVillager rand out v (E name f) = do 
+  let (eaten, rand')     = werewolfEatsAVillager v rand
+  let (cont, (o, v'))    = f v [[eaten]]
   input <- getLine
-  let (cont, (o, v'))    = f v [input]
-  let (eaten, rand')     = werewolfEatsAVillager v' rand
-  let (cont', (o', v'')) = run cont v' [eaten:[]]
+  let (rand', killed) = villagersVote (1,head input) v' rand
+  let (cont', (o', v'')) = run cont v' [[killed]]
   case endGame v' of
     None       -> hPutStrLn out (concat $ intersperse "\n" (o ++ o')) >> 
                   playVillager rand' out v'' cont'
