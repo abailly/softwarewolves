37bd040a8c4f91abfb212ee083168780cc3c5bc3
Arnaud Bailly <Arnaud.Bailly@polyspot.com>
2011-12-01 11:29:27 +0100

    eat a villager

---
+++ GameEngine.hs
@@ -0,0 +1,48 @@
+module GameEngine where
+import Test.HUnit
+import IO(stderr)
+import System.Exit
+import System.IO
+import Data.List
+
+-- CODE 
+eat villager (alive, dead) = (delete villager alive, villager : dead)
+
+-- TESTS
+
+gameEngineTests = TestList [
+  "when a player is designated for eating, it is removed from the villagers and added to dead" ~:
+  TestList [
+    eat 'B' ("BCD","") ~?= ("CD","B"),
+    eat 'C' ("BCD","E") ~?= ("BD","CE")  
+    ]
+  ]
+                  
+newtype Tests = T {unT :: Test}
+
+data TestCount = TestCount Int Test
+
+tests = T $ test [
+  gameEngineTests
+                 ]
+        
+runAllTests tests = do putStrLn "Running test suite: "
+                       putStrLn (show tests)
+                       counts <- runTest (unT tests)
+                       case (errors counts + failures counts) of
+                         0 -> return ExitSuccess
+                         n -> return (ExitFailure n)
+
+instance Show Tests where
+  show t = show' "" t
+  
+show'  indent (T (TestCase _))    = ""
+show'  indent (T (TestList ts))   = concat $ (map (show' (' ':indent) . T) ts)
+show'  indent (T (TestLabel l t)) = indent ++ l ++ ":\n" ++ (show' indent (T t))
+
+runTest :: Test -> IO Counts
+runTest  t = do (counts, _) <- runTestText (putTextToHandle stderr False) t
+		return counts
+
+
+
